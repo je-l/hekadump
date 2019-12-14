8bcfb4b626cbfdfb049bd03cec4cca477c757ab3
je-l <je-l@users.noreply.github.com>
2019-12-14 15:55:16 +0200

    Add parsing for rent, improve tests

--- lib/hekadump.ml
+++ lib/hekadump.ml
@@ -15,11 +15,11 @@ type pagination_crawl_result =
     next_page   : string option;
   }
 
-type exact_or_varying = Uniform of int | MinMax of int * int
+type int_or_int_range = Uniform of int | MinMax of int * int
 
 
 (** Some houses list floor count as range, e.g. 3-4 *)
-type floor_count = exact_or_varying
+type floor_count = int_or_int_range
 
 (** Single house building, e.g. https://www.hekaoy.fi/fi/asunnot/kohteet/hilda-flodinin-kuja-2 *)
 type parsed_house =
@@ -31,7 +31,7 @@ type parsed_house =
 
 type apartment_size = Same of float | Varying of float * float
 
-type rent = exact_or_varying
+type rent = int_or_int_range
 
 (** Single row in the apartment "asuntojakauma" table *)
 type apartment =
@@ -41,13 +41,87 @@ type apartment =
     rent           : rent;
   }
 
-let parse_apartment_table_row (n : element node) : string =
+(*
+First remove whitespace, then parse the min/max pair:
+
+1 100 - 1 200 --> 1100-1200 --> [| 1100-1200; 1100; 1200|]
+*)
+let parse_floor_text (text : string) : floor_count option =
+  let whitespace = Pcre.regexp "\\s+" in
+  let sub _ = "" in
+  let cleaned_text = Pcre.substitute ~rex:whitespace ~subst:sub text in
+  let regex = Pcre.regexp "(\\d+)(?:–|-)(\\d+)" in
+  let groups = try Some (Pcre.extract ~rex:regex cleaned_text)
+    with Not_found -> None in
+
+  match groups with
+    | Some [| _; left; right|] ->
+        Some (MinMax (int_of_string left, int_of_string right))
+    | None -> (match int_of_string_opt cleaned_text with
+        None -> None
+      | Some e -> Some (Uniform e))
+    | _ -> None
+
+(* Pervasives.float_of_string only parses floats with dot*)
+let float_of_string_finnish_opt (text : string) : float option =
+  let comma = Pcre.regexp "," in
+  let sub _ = "." in
+  let replaced = Pcre.substitute ~rex:comma ~subst:sub text in
+  float_of_string_opt replaced
+
+let float_of_string_finnish (text : string) : float =
+  match float_of_string_finnish_opt text with
+      None -> failwith (sprintf "cannot parse: %s\n" text)
+    | Some t -> t
+
+let parse_apartment_sizes (text : string) : apartment_size option =
+  let regex = Pcre.regexp "(\\d+,\\d+) - (\\d+,\\d+)" in
+  let groups = try Some (Pcre.extract ~rex:regex text)
+    with Not_found -> None in
+
+  match groups with
+    | Some [| _; minimum; maximum|] ->
+        Some (Varying (float_of_string_finnish minimum, float_of_string_finnish maximum))
+    | _ -> (match float_of_string_finnish_opt text with
+        None -> failwith (sprintf "text: %s\n" text)
+      | Some e -> Some (Same e))
+
+let parse_apartment_table_row (n : element node) : apartment =
   let cells = select "td" n in
   match to_list cells with
-      [typ; _; _; _] -> (match leaf_text typ with Some t -> t | None -> failwith "unexpected empty table cell")
+      [typ; sizes_text; count_text; raw_rent] -> begin
+        let residence_type = match leaf_text typ with
+            Some t -> t
+          | None -> failwith "unexpected empty table cell" in
+        let count = match leaf_text count_text with
+            Some tee -> begin
+              match int_of_string_opt (String.trim tee) with
+                  Some t -> t
+                | None -> failwith "non int apartment count"
+              end
+          | None -> failwith "unexpected " in
+
+        let sizes_raw = match leaf_text sizes_text with
+            None -> failwith "apartment sizes cell is empty"
+          | Some t -> t in
+        let sizes = match parse_apartment_sizes sizes_raw with
+            None -> failwith "cannot parse apartment"
+          | Some s -> s in
+        let parsed_rent = match leaf_text raw_rent with
+            None -> failwith "empty rent cell"
+          | Some t -> t in
+        let rent = match parse_floor_text parsed_rent with
+            None -> failwith "cannot parse rent"
+          | Some t -> t in
+        { residence_type;
+          sizes;
+          count;
+          rent
+        }
+        end
     | _ -> failwith "unexpected column count"
 
-let parse_apartment_table (html : soup node) : string list =
+let parse_apartment_table (html : soup node) : apartment list =
   let rows = select ".asuntojakauma-container tbody tr" html in
   List.map parse_apartment_table_row (to_list rows)
 
@@ -103,20 +177,8 @@ let find_integer html selector =
       None -> failwith (sprintf "cannot parse int from selector %s" selector)
     | Some t -> t
 
-let parse_floor_text (text : string) : floor_count option =
-  let regex = Pcre.regexp "(\\d+) – (\\d+)" in
-  let groups = try Some (Pcre.extract ~rex:regex text)
-    with Not_found -> None in
-
-  match groups with
-    | Some [| _; left; right|] ->
-        Some (MinMax (int_of_string left, int_of_string right))
-    | _ -> match int_of_string_opt text with
-        None -> None
-      | Some e -> Some (Uniform e)
-
-(** Floor count CSS classes are very misleading: either one of these two is
-used. The text content might look something like "4 - 6" *)
+(** Floor count CSS classes are misleading: either one of these two is used.
+The text content might look something like "4 - 6" *)
 let parse_floor_count (html : soup node) : floor_count option =
   let element = match select_one ".field--name-field-num-floors-min .field__item" html with
     None -> (match select_one ".field--name-field-num-floors-max .field__item" html with
@@ -143,7 +205,7 @@ let fetch_house (url : string) : parsed_house Lwt.t =
 
   let table = parse_apartment_table html in
 
-  List.iter (fun a -> printf "val: %s\n" a) table;
+  List.iter (fun a -> printf "val: %s\n" a.residence_type) table;
 
   let floor_count = match parse_floor_count html with
       None -> failwith (sprintf "cannot parse floor count for %s" url)
--- test/hekadump_test.ml
+++ test/hekadump_test.ml
@@ -1,20 +1,30 @@
 let test_uniform_floor_count () =
   match Hekadump.parse_floor_text "123" with
     | Some (Hekadump.Uniform t) -> Alcotest.(check int) "floor count" 123 t
-    | _ -> Alcotest.fail "unable to parse"
+    | Some (Hekadump.MinMax(_, _)) | None -> Alcotest.fail "unable to parse"
 
 let test_min_max_floor_count () =
   match Hekadump.parse_floor_text "4 – 7" with
     | Some (Hekadump.MinMax (l, r)) ->
         Alcotest.(check int) "max floor" 7 r;
         Alcotest.(check int) "min floor" 4 l;
-    | _ -> Alcotest.fail "unable to parse minmax"
+    | Some (Hekadump.Uniform(_)) -> Alcotest.fail "invalid uniform"
+    | None -> Alcotest.fail "unable to parse minmax"
+
+let test_min_max_rent () =
+  match Hekadump.parse_floor_text "1 100 - 1 200" with
+    | Some (Hekadump.MinMax (l, r)) ->
+        Alcotest.(check int) "max rent" 1200 r;
+        Alcotest.(check int) "min rent" 1100 l;
+    | Some (Hekadump.Uniform(_)) | None -> Alcotest.fail "cannot parse rent"
 
 let () =
   Alcotest.run "Hekadump"
     [
-      ( "parse_floor_text",
-        [ Alcotest.test_case "uniform" `Quick test_uniform_floor_count
-        ; Alcotest.test_case "uniform" `Quick test_min_max_floor_count]
+      ( "regex parsing",
+        [Alcotest.test_case "uniform floors" `Quick test_uniform_floor_count;
+        Alcotest.test_case "minmax floors" `Quick test_min_max_floor_count;
+        Alcotest.test_case "minmax rent" `Quick test_min_max_rent
+        ]
       )
     ]
